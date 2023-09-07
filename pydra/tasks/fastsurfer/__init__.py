bba03174edb4940e15d484fccb60eae10077467f
Tom Close <tom.g.close@gmail.com>
2023-09-07 18:46:43 +1000

    removed auto-generated warning

--- pydra/tasks/fastsurfer/__init__.py
+++ pydra/tasks/fastsurfer/__init__.py
@@ -3,35 +3,6 @@ This is a basic doctest demonstrating that the package and pydra can both be suc
 imported.
 
 >>> import pydra.engine
->>> import pydra.tasks.fastsurfer
+>>> from pydra.tasks.fastsurfer.v1 import fastsurfer
 """
-from warnings import warn
-from pathlib import Path
-
-pkg_path = Path(__file__).parent.parent
-
-try:
-    from ._version import __version__
-except ImportError:
-    raise RuntimeError(
-        "pydra-fastsurfer has not been properly installed, please run "
-        f"`pip install -e {str(pkg_path)}` to install a development version"
-    )
-if "nipype" not in __version__:
-    try:
-        from .auto._version import nipype_version, nipype2pydra_version
-    except ImportError:
-        warn(
-            "Nipype interfaces haven't been automatically converted from their specs in "
-            f"`nipype-auto-conv`. Please run `{str(pkg_path / 'nipype-auto-conv' / 'generate')}` "
-            "to generated the converted Nipype interfaces in pydra.tasks.fastsurfer.auto"
-        )
-    else:
-        n_ver = nipype_version.replace(".", "_")
-        n2p_ver = nipype2pydra_version.replace(".", "_")
-        __version__ += (
-            "_" if "+" in __version__ else "+"
-        ) + f"nipype{n_ver}_nipype2pydra{n2p_ver}"
-
-
-__all__ = ["__version__"]
+from ._version import __version__
